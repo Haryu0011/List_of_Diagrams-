6d67662b0175d4443e4886840221a4300292dc3f
Yanto <tommi5113@gmail.com>
2026-06-08 00:13:05 +0700

    Daftar semua diagram yang telah saya buat

---
+++ diagram/src/main/java/com/mycompany/diagram/Diagram.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.diagram;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Diagram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
